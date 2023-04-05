0759f265b794ea9a81b3d082f988336d4044f1f6
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-04 23:51:20 -0500

    Modify go route to return time

--- api/go.go
+++ api/go.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	fmt.Fprintf(w, "{\"message\": \"Hello from Go (api/go.go), \"date\": \"%s\" \"}", time.Now().UTC())
 }
