d652dfaed71348707588d7a7fc5037a461b99082
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-04-04 23:45:49 -0500

    Remove ruby route

--- api/ruby.rb
+++
@@ -1,9 +0,0 @@
-require 'cowsay'
- 
-Handler = Proc.new do |request, response|
-  name = request.query['name'] || 'World'
- 
-  response.status = 200
-  response['Content-Type'] = 'text/text; charset=utf-8'
-  response.body = Cowsay.say("Hello #{name}", 'cow')
-end
